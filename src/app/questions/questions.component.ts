7b06dec4eb9af0fb8e44b09809bbc662ed8b9c95
Arman-Gill <52617758+Arman-Gill@users.noreply.github.com>
2022-04-20 09:55:35 +0530

    added a timer

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,6 +1,7 @@
+import { ConstantPool } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 import { ReadingQuestionsService } from '../reading-questions.service';
-import { Questions } from './questionInterface';
 
 @Component({
   selector: 'app-questions',
@@ -14,6 +15,8 @@ export class QuestionsComponent implements OnInit {
   public current_question = 0;
   public score : number = 0;
   public testCompleted : boolean = false;
+  public interval$ :any;
+  counter = 60;
 
   constructor(private service: ReadingQuestionsService) { }
  
@@ -23,15 +26,20 @@ export class QuestionsComponent implements OnInit {
         .subscribe(data => this.questions = data.questions)
 
     this._user = localStorage.getItem('user')!;
+    this.startTimer()
 
   }
 
   changeQuestion(){
     if(this.current_question == (this.questions.length - 1)) {
       this.testCompleted = true;
+      this.stopTimer()
     }
     else{
-      this.current_question ++;
+      
+      this.current_question++;
+      this.resetTimer();
+      
     }
   }
 
@@ -45,7 +53,32 @@ export class QuestionsComponent implements OnInit {
 
   }
 
+  startTimer(){
+    this.interval$ = interval(1000)
+    .subscribe(val=>{
+      this.counter--;
+      if (this.counter===0){
+        this.changeQuestion()}
+      });
   
-  
+    
+
+    setTimeout(() => {
+      this.interval$.unsubscribe();
+    }, 1000000);
+
+  }
+
+  stopTimer(){
+    this.interval$.unsubscribe();
+    this.counter = 0;
+  }
+
+  resetTimer(){
+    this.stopTimer()
+    this.counter = 60;
+    this.startTimer();
+
+  }
 
 }
